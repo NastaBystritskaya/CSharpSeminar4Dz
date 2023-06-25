ccfd44ad810d0dd87783799f50c442f010b5b3d7
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-06-25 14:43:02 +0300

    выполнили задачу 27

---
+++ задача 27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе.
+void Main () {
+    string input = get_user_input();
+    int result = get_sum(input);
+    System.Console.WriteLine(result);
+}
+
+string? get_user_input () {
+    System.Console.WriteLine("Введите число: ");
+    string? input = System.Console.ReadLine();
+    return input;
+}
+
+int get_sum (string input) {
+    int sum = 0;
+    for(int i = 0; i < input.Length; i++) {
+        sum = sum + int.Parse(input[i].ToString());
+    }
+    return sum;
+}
+Main();
