b2f1d587f07cf4bda784577eabdda32410cca301
NastaBystritskaya <nasta.bystritskaya@gmail.com>
2023-06-27 14:21:23 +0300

    выполнили задачу 29

---
+++ задача 29/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран
+
+int[] massiv = new int[8];
+for (int i = 0; i<8; i++) {
+    massiv[i] = new Random().Next(0, 100);
+}
+System.Console.WriteLine(string.Join(", ", massiv));
